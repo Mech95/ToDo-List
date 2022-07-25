f0fecb7c985f59328b062b2abbc3560e6f714df9
NIKHIL <cc.nikhil@gmail.com>
2022-07-25 19:13:10 +0530

    ToDo List

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import './App.css';
+import{useState} from 'react'
+
+function App() {
+  const[toDos,setToDos]=useState([])
+ const[toDo,setToDo]=useState('')
+//Deleting obj
+ const index = toDos && toDos.findIndex(obj => obj.statusRemove === true);
+ // console.log(index);
+ if (index > -1) toDos && toDos.splice((index), 1);
+ //for Day
+  const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+   const date = new Date();
+   const day = dayNames[date.getDay()];
+
+  return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+
+        <br />
+        <h2 className="gradient-text">Hey, it's {day}</h2>
+      </div>
+      <div className="input">
+
+
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+       {
+       toDos.map((obj)=>{
+        return(
+        <div className="todo">
+          <div className="left">
+            <input onChange={(e)=>{
+              console.log(e.target.checked)
+              console.log(obj)
+
+              setToDos(toDos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                  obj2.status=e.target.checked
+                }
+                return obj2
+              }))
+
+            }} value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
+            
+          </div>
+
+          <div className="right">
+             <i onClick={(e) => {
+             let isdelete = window.confirm("Deleting ToDo permanently !");
+             if (isdelete) {
+                 e.target.value = true;
+                }
+             setToDos(toDos.filter((obj2) => {
+                if (obj2.id === obj.id) {
+                obj2.statusRemove = e.target.value;
+                }
+            return obj2;
+              }));
+          }} value={obj.statusRemove} className="fa fa-times" title="Remove"></i>
+         </div>
+         
+         
+
+        </div>)
+        })} 
+        {toDos.map((obj)=>{
+          if(obj.status){
+            return(
+              <h1>{obj.text}</h1>
+            )
+          }
+          return null
+        })}
+      </div>
+    </div>
+  );
+}
+
+
+  
+
+
+export default App;
